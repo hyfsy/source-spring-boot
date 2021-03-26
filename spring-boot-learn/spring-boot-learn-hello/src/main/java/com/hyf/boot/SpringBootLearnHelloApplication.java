407e06a42c97c971c5f175dde0a5035f1084a3b8
hyfeng <hyfsy@epoint.com.cn>
2021-03-26 20:29:10 +0800

    格式化修改

--- spring-boot-learn/spring-boot-learn-hello/src/main/java/com/hyf/boot/SpringBootLearnHelloApplication.java
+++ spring-boot-learn/spring-boot-learn-hello/src/main/java/com/hyf/boot/SpringBootLearnHelloApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootLearnHelloApplication
-{
+public class SpringBootLearnHelloApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLearnHelloApplication.class, args);
